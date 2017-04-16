46805905d13c9ff7effe7a8ac30988ad623bb877
Lil Six <geminijune08@gmail.com>
2017-04-17 01:20:36 +0800

    Touch button to play audio.

--- MusicBox/MusicBoxViewController.m
+++ MusicBox/MusicBoxViewController.m
@@ -11,6 +11,10 @@
 
 @interface MusicBoxViewController ()<AVAudioSessionDelegate> {
     AVAudioPlayer *audioPlayer;
+    NSMutableArray *audioForMutableArray;
+    NSArray *audioForArray;
+    NSUInteger *buttonTag;
+    
 }
 
 @end
@@ -20,21 +24,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSString *audioFilePath = [[NSBundle mainBundle] pathForResource:@"01.延俊出場" ofType:@"mp3"];
-    NSData *musicData = [[NSData alloc] initWithContentsOfFile:audioFilePath];
-    audioPlayer = [[AVAudioPlayer alloc] initWithData:musicData error:nil];
+//    for (int i = 0; i <= audioForMutableArray.count; i++) {
+//        NSString *audioFilePath = [[NSBundle mainBundle] pathForResource:audioForArray[3] ofType:@"mp3"];
+//        NSData *musicData = [[NSData alloc] initWithContentsOfFile:audioFilePath];
+//        audioPlayer = [[AVAudioPlayer alloc] initWithData:musicData error:nil];
+//    }
     
-    
-    for (int i = 0; i <= 3; i++) {
-        for (int j = 0; j <= 3; j++) {
-            UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(20 + (251 * j), 20 + (187 * i), 231, 167)];
+    for (int i = 0; i <= 15; i++) {
+//        for (int j = 0; j <= 3; j++) {
+            UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(20 + (251 * i), 20, 231, 167)];
+//            NSArray *nameForArray = @[@"延俊出場", @"法國貴婦", @"台灣阿姐", @"哪吒三太子"];
+//            [button setTitle:nameForArray[j] forState:UIControlStateNormal];
             [button setTitle:@"Button" forState:UIControlStateNormal];
             [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
             button.backgroundColor = [UIColor blackColor];
-            
+           
             [button addTarget:self action:@selector(audioPlay:) forControlEvents:UIControlEventTouchUpInside];
             [self.view addSubview:button];
-        }
+//        }
     }
 }
 
@@ -43,9 +50,62 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)audioPlay:(UIButton *)button {
-    NSLog(@"Audio playing.");
-    [audioPlayer play];
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+//        audioForMutableArray = [NSMutableArray array];
+
+        audioForArray = @[@"01.延俊出場", @"02.法國貴婦", @"03.台灣阿姐", @"04.哪吒三太子"];
+//        [audioForMutableArray addObjectsFromArray:array];
+//        NSLog(@"audioForMutableArray: %@", audioForMutableArray[0]);
+    }
+    return self;
+}
+
+
+
+- (IBAction)audioPlay:(UIButton *)button {
+    
+    button.selected = !button.selected;
+    if ([button.currentTitle isEqualToString:@"延俊出場"]) {
+        NSString *audioFilePath = [[NSBundle mainBundle] pathForResource:audioForArray[0] ofType:@"mp3"];
+        NSData *musicData = [[NSData alloc] initWithContentsOfFile:audioFilePath];
+        audioPlayer = [[AVAudioPlayer alloc] initWithData:musicData error:nil];
+        [audioPlayer play];
+//        NSLog(@"button.tag = %ld", button);
+        button.selected = !button.selected;
+        
+    } else if ([button.currentTitle isEqualToString:@"法國貴婦"]) {
+        NSString *audioFilePath = [[NSBundle mainBundle] pathForResource:audioForArray[1] ofType:@"mp3"];
+        NSData *musicData = [[NSData alloc] initWithContentsOfFile:audioFilePath];
+        audioPlayer = [[AVAudioPlayer alloc] initWithData:musicData error:nil];
+        [audioPlayer play];
+        NSLog(@"button.tag = %ld", button.tag);
+        button.selected = !button.selected;
+    }
+    
+    
+//    NSLog(@"Audio play: %@.mp3", );
+//    button.selected = !button.selected;
+//    if (button.selected) {
+//        button.selected = !button.selected;
+//        [audioPlayer play];
+//    }
+//    else {
+//        [audioPlayer pause];
+//        button.selected = !button.selected;
+//    }
+    
+//    if ([audioPlayer play] && [button touchesCancelled:<#(nonnull NSSet<UITouch *> *)#> withEvent:<#(nullable UIEvent *)#>) {
+//        [audioPlayer pause];
+////        [audioPlayer prepareToPlay];
+//        NSLog(@"Pause.");
+//    } else {
+////        [audioPlayer prepareToPlay];
+//        [audioPlayer play];
+//        NSLog(@"Play.");
+//    }
 }
 
 
